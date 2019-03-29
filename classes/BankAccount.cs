bb8720eef3d5eb1f10fcd7230c7eac09bff4a395
Jeremy Robertson <jrobertson.88@gmail.com>
2019-03-28 22:30:53 -0500

    Add making withdrawals and deposits

--- classes/BankAccount.cs
+++ classes/BankAccount.cs
@@ -25,14 +25,30 @@ namespace classes
 
         public void MakeDeposit(decimal amount, DateTime date, string note)
         {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive");
+            }
+            var deposit = new Transaction(amount, date, note);
+            allTransactions.Add(deposit);
         }
 
         public void MakeWithdrawal(decimal amount, DateTime date, string note)
         {
+            if (amount<=0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawal must be positive");
+            }
+            if (Balance- amount < 0)
+            {
+                throw new InvalidOperationException("Not sufficient funds for this withdrawal");
+            }
+            var withdrawal = new Transaction(-amount, date, note);
+            allTransactions.Add(withdrawal);
         }
         public BankAccount(string name, decimal initialBalance){
             this.Owner = name;
-            this.Balance = initialBalance;
+            MakeDeposit(initialBalance, DateTime.Now, "Initial balance");
             this.Number = accountNumberSeed.ToString();
             accountNumberSeed++;
         }
